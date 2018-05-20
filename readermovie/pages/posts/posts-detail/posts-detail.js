31491666a109b61e9f9949d35150555c5c9a01ad
HeiLiu <969403750@qq.com>
2018-05-20 23:36:33 +0800

    在详情页面增加简单的音乐播放关闭

--- readermovie/pages/posts/posts-detail/posts-detail.js
+++ readermovie/pages/posts/posts-detail/posts-detail.js
@@ -9,7 +9,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    isPlayingMusic:false
   },
 
   /**
@@ -136,5 +136,38 @@ Page({
       icon: "success",
       duration: 1000
     });
+  },
+  onMusicTap:function(){
+    let currentPostId = this.data.currentPostId;
+    let postData = postData.postList[currentPostId];
+    let isPlayingMusic = this.data.isPlayingMusic;
+    if(isPlayingMusic){
+      wx.pauseBackgroundAudio();
+      this.setData({
+        isPlayingMusic : false
+      })
+    }else{
+      wx.playBackgroundAudio({
+        dataUrl:'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46',
+        title:postData.music.title,
+        coverImg:''
+      })
+      this.setData({
+        isPlayingMusic : true
+      })
+    }
+  },
+  onShareAppMessage(){
+    // 居然没有用
+    return {
+      title:'今晚打老虎',
+      path:`/pages/index/index`,
+      success:function(res){
+
+      },
+      fail:function(res){
+
+      }
+    }
   }
 })
