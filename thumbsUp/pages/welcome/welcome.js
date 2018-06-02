bf9ad8d80d85f691a6ac9f0b686c215aeeef1f66
HeiLiu <969403750@qq.com>
2018-06-02 17:16:49 +0800

    welcome 页面重构 样式重写 增加动画

--- thumbsUp/pages/welcome/welcome.js
+++ thumbsUp/pages/welcome/welcome.js
@@ -1,11 +1,31 @@
+
 Page({
-    onTap(){
-        // wx.navigateTo({
-        //     url:"../posts/post"
-        // });
+    data: {
+        rotate: ''
+    },
+    onTap() {
+        this.setData({
+            rotate: true
+        })
+        setTimeout(function () {
+            navigateTo();
+        }, 2000)
+    },
+
+    navigateTo:function() {
         wx.redirectTo({
             // 不需要返回上一层
-            url:"../index/index"
+            url: "../index/index"
         })
+        setTimeout(navigateTo(),2000)
+    },
+    
+    onLoad() {
+        setTimeout(() => {
+            this.setData({
+                rotate: true
+            })
+            navigateTo();
+        }, 5000)
     }
 })
